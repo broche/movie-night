448a0bb48f27e2d465cf3dd3d5e9fbb1fc547678
ben.roche@onesky.com <ben.roche@onesky.com>
2023-02-14 23:19:42 -0600

    Added APP_BASE_HREF

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CommonModule } from '@angular/common';
+import { APP_BASE_HREF, CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
 
@@ -19,7 +19,9 @@ import { HttpClientModule } from '@angular/common/http';
     CommonModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/movie-night' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
